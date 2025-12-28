b8d93d4b3377ae73d40b66ce0b648cb017ee5766
Michael Amelian <mykovanov@gmail.com>
2025-12-28 02:15:58 -0500

    main updated for sql functionality

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from db import engine
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/db-health")
+def db_health():
+    with engine.connect() as conn:
+        value = conn.execute(text("SELECT 1")).scalar_one()
+
+    return {"db": "ok", "select": value}
